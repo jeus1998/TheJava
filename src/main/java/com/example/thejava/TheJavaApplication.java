8aaa382707a9c96f5cece74a30c0690f34a88dcc
jeus1998 <zeus20240228@gmail.com>
2024-10-08 20:06:37 +0900

    [2024:10:08] TheJava create

---
+++ src/main/java/com/example/thejava/TheJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.thejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thejava/TheJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
