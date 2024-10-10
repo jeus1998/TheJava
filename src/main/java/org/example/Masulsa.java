9143568690674a49264da89803da4bceeaa1e9f5
jeus1998 <zeus20240228@gmail.com>
2024-10-10 17:47:12 +0900

    [2024:10:10] 바이트 코드 조작: JavaAgent 활용 Application 코드

--- src/main/java/org/example/Masulsa.java
+++ src/main/java/org/example/Masulsa.java
@@ -4,20 +4,32 @@ package org.example;
 import java.io.File;
 import java.io.IOException;
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 public class Masulsa {
     public static void main(String[] args) {
-        try {
-            // 메소드 재정의
-            new ByteBuddy().redefine(Moja.class)
-                       .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
-                       .make().saveIn(new File("C:/Study/WhiteShipLecture/Java/target/classes/"));
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-
         System.out.println(new Moja().pullOut());
     }
 }
+
+/* 순서에 너무 의존적
+public static void main(String[] args) {
+    ClassLoader classLoader = Masulsa.class.getClassLoader();
+    TypePool typePool = TypePool.Default.of(classLoader);
+
+    try {
+        // 메소드 재정의
+        new ByteBuddy().redefine(
+                    typePool.describe("org.example.Moja").resolve(),
+                    ClassFileLocator.ForClassLoader.of(classLoader))
+                   .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+                   .make().saveIn(new File("C:/Study/WhiteShipLecture/Java/target/classes/"));
+    }
+    catch (IOException e){
+        e.printStackTrace();
+    }
+    System.out.println(new Moja().pullOut());
+}
+ */
